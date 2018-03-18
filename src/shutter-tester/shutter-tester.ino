f0e465bbb8e215d98fdc08b1e33f573eb260ef3f
wucke13 <wucke13@gmail.com>
2018-03-18 13:48:29 +0100

    1. removed license header, due to redundancy 2. linted/indented according to arduino standards (CTRL+T in Arduino IDE) 3. put initialise lcd call into setup() function 4. removed suspicious while loop in setup() function 5. changed delay based random toggle prevention to while based 6. changed start_time > 0 to start_time != 0, because start_time cannot be less than 0, so its functional equivalent but more clear what shall be the condition

--- src/shutter-tester/shutter-tester.ino
+++ src/shutter-tester/shutter-tester.ino
@@ -1,31 +1,5 @@
-/*
- * MIT License
- *
- * Copyright (c) 2018 c-s-1
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
- */
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
-
-// Initialise an LCD
-LiquidCrystal_I2C lcd(0x27, 20, 4);
 
 // Version
 const String version = "v0.1";
@@ -50,53 +24,46 @@ String maxtime_s = "";
 boolean light = false;
 
 void setup() {
+  // Initialise an LCD
+  LiquidCrystal_I2C lcd(0x27, 20, 4);
   // Initialise pin for IR sensor input and attach an interrupt to it
   pinMode(interruptPin, INPUT_PULLUP);
   attachInterrupt(digitalPinToInterrupt(interruptPin), exp, CHANGE);
   /*
-   * Initialise pins for buttons.
-   * WARNING: don't forget to define it as an input w/ pullup. If you
-   * don't you'll very likely fry your Arduino!
-   */
+     Initialise pins for buttons.
+     WARNING: don't forget to define it as an input w/ pullup. If you
+     don't you'll very likely fry your Arduino!
+  */
   pinMode(buttonPin, INPUT_PULLUP);
   pinMode(lightButtonPin, INPUT_PULLUP);
   // Initialise LCD and print intro
   lcd.init();
   lcd.backlight();
   light = true;
-  lcd.setCursor(0,0);
+  lcd.setCursor(0, 0);
   lcd.print("=== Shutter Test ===");
-  lcd.setCursor(0,1);
+  lcd.setCursor(0, 1);
   lcd.print("Version " + version);
-  lcd.setCursor(0,2);
+  lcd.setCursor(0, 2);
   lcd.print("Press reset to");
-  lcd.setCursor(0,3);
+  lcd.setCursor(0, 3);
   lcd.print("start test.");
-  // Catch reset button press
-  while (digitalRead(buttonPin) == HIGH)
-  {
-    if (digitalRead(lightButtonPin) == LOW)
-    {
-      toggle_light();
-      delay(500);
-    }
-  }
   setup_lcd();
 }
 
 void setup_lcd() {
   /*
-   * Method for setting up the LCD for the next round
-   * of shutter tests.
-   */
+     Method for setting up the LCD for the next round
+     of shutter tests.
+  */
   lcd.clear();
-  lcd.setCursor(0,0);
+  lcd.setCursor(0, 0);
   lcd.print("Exp. #" + String(exposure));
-  lcd.setCursor(0,1);
+  lcd.setCursor(0, 1);
   lcd.print("Speed:");
-  lcd.setCursor(0,2);
+  lcd.setCursor(0, 2);
   lcd.print("Min:");
-  lcd.setCursor(0,3);
+  lcd.setCursor(0, 3);
   lcd.print("Max:");
 }
 
@@ -110,8 +77,8 @@ void loop() {
   if (digitalRead(lightButtonPin) == LOW)
   {
     toggle_light();
-    // Delay after toggling to avoid "flapping".
-    delay(500);
+    // Endless loop until button is released.
+    while (!digitalRead(lightButtonPin));
   }
   // If a complete shutter release and reset was detected,
   // print the measured result.
@@ -119,7 +86,7 @@ void loop() {
   {
     String s = "";
     exposure++;
-    if (start_time > 0 && start_time < end_time)
+    if (start_time != 0 && start_time < end_time)
     {
       // Calculate time difference between start and end of exposure in microseconds.
       unsigned long timed = end_time - start_time;
@@ -154,28 +121,28 @@ void loop() {
 
 void print_result(String s) {
   /*
-   * Method for LCD output of results.
-   */
-  lcd.setCursor(6,0);
+     Method for LCD output of results.
+  */
+  lcd.setCursor(6, 0);
   lcd.print(String(exposure));
-  lcd.setCursor(7,1);
+  lcd.setCursor(7, 1);
   lcd.print("            ");
-  lcd.setCursor(7,1);
+  lcd.setCursor(7, 1);
   lcd.print(s + "s");
-  lcd.setCursor(5,2);
+  lcd.setCursor(5, 2);
   lcd.print("               ");
-  lcd.setCursor(5,2);
+  lcd.setCursor(5, 2);
   lcd.print(mintime_s + "s");
-  lcd.setCursor(5,3);
+  lcd.setCursor(5, 3);
   lcd.print("               ");
-  lcd.setCursor(5,3);
+  lcd.setCursor(5, 3);
   lcd.print(maxtime_s + "s");
 }
 
 void exp() {
   /*
-   * Method called by interrupt attached to IR sensor output.
-   */
+     Method called by interrupt attached to IR sensor output.
+  */
   unsigned long temp_time = micros();
   // Pin is LOW if IR sensor detects IR (i. e. shutter is open).
   if (digitalRead(interruptPin) == LOW)
@@ -195,8 +162,8 @@ void exp() {
 
 void toggle_light() {
   /*
-   * Method that toggles the LCD's backlight.
-   */
+     Method that toggles the LCD's backlight.
+  */
   if (light)
   {
     lcd.noBacklight();
@@ -211,15 +178,15 @@ void toggle_light() {
 
 void reset() {
   /*
-   * Method for resetting the LCD's content and some variables.
-   */
-  lcd.setCursor(6,0);
+     Method for resetting the LCD's content and some variables.
+  */
+  lcd.setCursor(6, 0);
   lcd.print("0");
-  lcd.setCursor(7,1);
+  lcd.setCursor(7, 1);
   lcd.print("            ");
-  lcd.setCursor(5,2);
+  lcd.setCursor(5, 2);
   lcd.print("               ");
-  lcd.setCursor(5,3);
+  lcd.setCursor(5, 3);
   lcd.print("               ");
   exposed = false;
   exposure = 0;
